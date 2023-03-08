720cfb16f6547ff1f9504671cc7874b81ac2f615
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-08 12:01:56 +0000

    Navbar is working

--- social-media-app/src/components/routing/Routing.js
+++ social-media-app/src/components/routing/Routing.js
@@ -6,6 +6,7 @@ import Login from '../Login/Login'
 import Home from '../Home/Home'
 import Signup from "../Signup/Signup";
 import PrivateRoute from "./PrivateRoute";
+import Logout from "../Logout/Logout";
 
 const Routing = () => {
     return (
@@ -13,6 +14,8 @@ const Routing = () => {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login  />} />
             <Route path='/signup' element={<Signup />} />
+            <Route path ='/dashboard' element={<Dashboard/>}/>
+            <Route path ='/logout' element={<Logout/>}/>
             <Route element={<PrivateRoute  />}>
                 <Route path='/dashboard' element={<Dashboard/>} />
             </Route>
