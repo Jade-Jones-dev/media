d488bfa920ae211dfb029f4fbb768e982f4ebb55
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-08 12:49:11 +0000

    isAdmin test

--- social-media-app/src/components/Dashboard/Dashboard.js
+++ social-media-app/src/components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import './Dashboard.css'
 export default function Dashboard() {
 	const name=localStorage.getItem('name')
+	const isAdmin=localStorage.getItem('isAdmin')
 	return (
 		<div className='dashboard-wrapper'>
 			<h2>{name} Dashboard</h2>
+			<h2>{name} isAdmin is {isAdmin}</h2>
 		</div>
 	);
 }
