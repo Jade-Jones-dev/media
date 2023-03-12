3ed2e481bf917bbbd13b5b598897c0eb422a7b5a
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-12 22:43:00 +0000

    routes not working correctly

--- social-media-app/src/components/Dashboard/Dashboard.js
+++ social-media-app/src/components/Dashboard/Dashboard.js
@@ -1,20 +1,22 @@
-import {useState, useEffect} from 'react'
-import './Dashboard.css'
+import {useState, useEffect} from "react";
+import "./Dashboard.css";
 import {useNavigate, Link} from "react-router-dom";
 
 export default function Dashboard({user}) {
-	const navigate = useNavigate()
-	const name=localStorage.getItem('name')
-	const adminValue =localStorage.getItem('isAdmin')
+	const navigate = useNavigate();
+	const name = localStorage.getItem("name");
+	const adminValue = localStorage.getItem("isAdmin");
 
-	const [isAdmin, setIsAdmin] =useState()
-	const [messages, setMessages] = useState([])
-  
+	const [isAdmin, setIsAdmin] = useState();
+	const [messages, setMessages] = useState([]);
+
 	useEffect(() => {
-		const adminValue = localStorage.getItem('isAdmin');
+		const adminValue = localStorage.getItem("isAdmin");
 		const isAdmin = JSON.parse(adminValue);
 		setIsAdmin(isAdmin);
-	  }, []);
+	}, []);
+
+
 
 	//   useEffect(() => {
 	// 	fetch("http://127.0.0.1:8080/api/auth/login")
@@ -23,58 +25,51 @@ export default function Dashboard({user}) {
 	// 	  .catch((error) => console.error(error));
 	//   }, []);
 
+	useEffect(() => {
+		fetchMessages();
+	}, []);
 
-		  useEffect(() => {
-			fetchMessages();
-		}, []);
+	function fetchMessages() {
+		fetch("http://127.0.0.1:8080/api/messages/")
+			.then((response) => response.json())
+			.then((data) => setMessages(data))
+			.catch((error) => console.error(error));
+	}
 
-		
-	
-		function fetchMessages() {
-			fetch("http://127.0.0.1:8080/api/messages/")
-		  .then((response) => response.json())
-		  .then((data) => setMessages(data))
-		  .catch((error) => console.error(error));
-		}
+	function handleClick() {
+		navigate("/createMessage");
+	}
 
-		function handleClick(){
-			navigate('/createMessage')
-		}
-	
 	return (
 		<div className='dashboard-wrapper'>
 			{!isAdmin && (
-			<>
-			<button  className='btn' onClick={handleClick}>Create message</button>
-			<div className='messages'>
-			{/* <button>Create message</button> */}
-					{messages.map((message, index) => {
-			
-						return <div className='message'key={message.id}>
-							<h3 >{message.title}</h3>
-							<Link className='btn' to={`/viewMessage/${message.id}`} >
-  view
-</Link>
-
-							
-						</div>
-
-					})}
-				
-
-
-			</div>
-
-			</>
-		)}
-		{isAdmin && (
-			<>
-			<h2>{name} This is an Admin Dashboard</h2>
-			<h2>{name} isAdmin is {adminValue}</h2>
-			</>
-		)}
-			
-			
+				<>
+					<button className='btn' onClick={handleClick}>
+						Create message
+					</button>
+					<div className='messages'>
+						{/* <button>Create message</button> */}
+						{messages.map((message, index) => {
+							return (
+								<div className='message' key={message.id}>
+									<h3>{message.title}</h3>
+									<Link className='btn' to={`/viewMessage/${message.id}`}>
+										view
+									</Link>
+								</div>
+							);
+						})}
+					</div>
+				</>
+			)}
+			{isAdmin && (
+				<>
+					<h2>{name} This is an Admin Dashboard</h2>
+					<h2>
+						{name} isAdmin is {adminValue}
+					</h2>
+				</>
+			)}
 		</div>
 	);
 }
