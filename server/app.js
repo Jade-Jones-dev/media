1e255e4088b1ceee656e7540c36e8830af244370
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-12 21:04:03 +0000

    update message form filling

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser')
 dotenv.config();
 
 const userRoutes = require("./routes/user");
@@ -22,6 +23,7 @@ var corsOptions = {
 	origin: "http://localhost:3000",
 };
 app.use(cors(corsOptions));
+app.use(bodyParser.json())
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
--- social-media-app/src/components/Messages/UpdateMessage.js
+++ social-media-app/src/components/Messages/UpdateMessage.js
@@ -4,14 +4,16 @@ import { useParams } from 'react-router-dom';
 const UpdateMessage = () => {
 	const { id } = useParams();
 	const [message, setMessage] = useState({});
-	// const [title, setTitle] = useState("");
-	// const [body, setBody] = useState("");
+	const [title, setTitle] = useState("");
+	const [body, setBody] = useState("");
 
 	useEffect(() => {
 		fetch(`http://localhost:8080/api/messages/${id}`)
 		  .then((response) => response.json())
 		  .then((data) => setMessage(data))
 		  .catch((error) => console.error(error));
+		  setTitle(message.title)
+		  setBody(message.body)
 	  }, [id]);
 
 	
@@ -19,17 +21,17 @@ const UpdateMessage = () => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		// const newMessage = {
-		// 	body,
-		// 	title,
-		// };
+		const newMessage = {
+			body,
+			title,
+		};
 
-		fetch(`http://127.0.0.1:8080/api/messages${id}`, {
-			method: "post",
+		fetch(`http://127.0.0.1:8080/api/messages/${id}`, {
+			method: "put",
 			headers: {
 				"Content-type": "application/json",
 			},
-			// body: JSON.stringify(newMessage),
+			 body: JSON.stringify({newMessage}),
 		})
 			.then((res) => res.json())
 			.then((data) => console.log(data))
@@ -40,11 +42,11 @@ const UpdateMessage = () => {
             <h2>Update message</h2>
 			<label>
 				<p>Title</p>
-				<input type='title' value={message.title} />
+				<input type='title' defaultValue={title} onChange={(event) => setTitle(event.target.value)}/>
 			</label>
 			<label>
 				<p>Message</p>
-				<textarea type='text' value={message.body}  />
+				<textarea type='text' defaultValue={body} onChange={(event) => setBody(event.target.value)} />
 			</label>
 
 			<div>
